4e50b93735cc70c79bf143504ccf21365eb82f5b
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-09-10 19:27:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+        var btnText = this.textContent;
+        music(btnText);
+    
+    });
+
+}
+
+
+
+
+document.addEventListener("keypress",function(e){
+
+    music(e.key);
+
+});
+
+
+
+
+
+function music(pressedKey){
+
+    buttonAnimation(pressedKey);
+
+    switch(pressedKey){
+        case "w":
+            var audio1 = new Audio('./sounds/crash.mp3');
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio('./sounds/kick-bass.mp3');
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio('./sounds/snare.mp3');
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio('./sounds/tom-1.mp3');
+            audio4.play();
+            break;
+        case "j":
+            var audio5 = new Audio('./sounds/tom-2.mp3');
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio('./sounds/tom-3.mp3');
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio('./sounds/tom-4.mp3');
+            audio7.play();
+            break
+        default : 
+            console.log(pressedKey + " this is not valid key   USE(w,a,s,d,j,k,l)");
+    }
+}
+
+
+function buttonAnimation(key){
+    
+    var activeButton = document.querySelector("."+key);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 200);
+
+}
